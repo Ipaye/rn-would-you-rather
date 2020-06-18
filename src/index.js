5573862e0700969b360a5aba9b5f3efa856f5815
Ipaye Alameen <Akinkunmi007@gmail.com>
2020-06-18 13:20:06 +0100

    WIP: setup router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import './index.css'
 import App from './components/App'
@@ -7,7 +8,9 @@ import App from './components/App'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 )
