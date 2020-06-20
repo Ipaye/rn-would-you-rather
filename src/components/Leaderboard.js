31532fb2aa83f897920605d77c716c14beafcec3
Ipaye Alameen <Akinkunmi007@gmail.com>
2020-06-20 17:41:03 +0100

    UI: Finished building UI for leaderboard

--- src/components/Leaderboard.js
+++ src/components/Leaderboard.js
@@ -11,6 +11,96 @@ class Leaderboard extends Component {
     return (
       <div className="container">
         <Navigation />
+
+        <div class="page-header mt-5">
+          <div class="content">
+            <h1 className="heading-title">Leaderboard</h1>
+            <p>View all the details and ranking of all the users</p>
+          </div>
+        </div>
+
+        <div class="columns">
+          <div class="column is-4">
+            <div class="box">
+              <div class="box-content">
+                <div class="box-left">
+                  <div>My Rank</div>
+                  <h4>3rd Place</h4>
+                </div>
+                <div class="box-right">
+                  <div>My Score</div>
+                  <h4>12</h4>
+                </div>
+              </div>
+            </div>
+
+            <div class="box">
+              <div class="box-content profile">
+                <img src="/assets/avatars/1.png" className="avatar" alt="" />
+
+                <div class="profile-content">
+                  <h2>Fellani Max payne</h2>
+                  <h5>@fgelaniPaymen</h5>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="column">
+            <div class="box">
+              <table class="table is-fullwidth">
+                <thead>
+                  <tr>
+                    <th>
+                      <abbr title="Position">Pos</abbr>
+                    </th>
+                    <th>Fullname</th>
+                    <th>
+                      <span title="Answered Questions">Answered</span>
+                    </th>
+                    <th>
+                      <abbr title="Points gotten">Points</abbr>
+                    </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th>1</th>
+                    <td style={{ width: '100px' }}>
+                      <div class="table-user">
+                        <div class="avatar">
+                          <img src="/assets/avatars/1.png" alt="" />
+                        </div>
+                        <div class="content ml-3">
+                          <h4>Ipaye Alameen</h4>
+                          <p>@alameen</p>
+                        </div>
+                      </div>
+                    </td>
+                    <td>38</td>
+                    <td>23</td>
+                  </tr>
+                  <tr>
+                    <th>1</th>
+                    <td style={{ width: '100px' }}>
+                      <div class="table-user">
+                        <div class="avatar">
+                          <img src="/assets/avatars/1.png" alt="" />
+                        </div>
+                        <div class="content ml-3">
+                          <h4>Ipaye Alameen</h4>
+                          <p>@alameen</p>
+                        </div>
+                      </div>
+                    </td>
+                    <td>38</td>
+                    <td>23</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
       </div>
     )
   }
