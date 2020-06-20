589323498995042f1149a68fbb297e9724ce7d86
Ipaye Alameen <Akinkunmi007@gmail.com>
2020-06-20 23:11:40 +0100

    Refactor: Did the main Page

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import { connect } from 'react-redux'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import LoadingBar from 'react-redux-loading'
 
-function App() {
-  return <div className="App"></div>
+import { handleInitialData } from '../actions/shared'
+
+// Pages
+import Login from './Login'
+import Home from './Home'
+import QuestionDetails from './QuestionDetails'
+import NewQuestion from './NewQuestion'
+import Leaderboard from './Leaderboard'
+
+class App extends React.Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData())
+  }
+  render() {
+    return (
+      <Router>
+        <Fragment>
+          <LoadingBar />
+
+          <Route path="/" exact component={Login} />
+          <Route path="/dashboard" exact component={Home} />
+          <Route path="/question/:id" component={QuestionDetails} />
+          <Route path="/add" component={NewQuestion} />
+          <Route path="/leaderboard" component={Leaderboard} />
+        </Fragment>
+      </Router>
+    )
+  }
 }
 
-export default App
+export default connect()(App)
