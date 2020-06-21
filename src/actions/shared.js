12322ce6967412e26769cf923141998eac0258a6
Ipaye Alameen <Akinkunmi007@gmail.com>
2020-06-21 01:46:12 +0100

    Refactor: add fucntionality to keep logged in user logged in

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -3,6 +3,7 @@ import { showLoading, hideLoading } from 'react-redux-loading'
 import { _getQuestions, _getUsers } from '../utils/_data'
 import { getUsers } from '../actions/users'
 import { getQuestions } from '../actions/questions'
+import { loginUser } from './auth'
 
 export function handleInitialData() {
   return (dispatch) => {
@@ -10,6 +11,10 @@ export function handleInitialData() {
 
     return Promise.all([_getQuestions(), _getUsers()]).then(([questions, users]) => {
       dispatch(getUsers(users))
+      const activeUser = localStorage.getItem('active-user')
+      if (activeUser) {
+        dispatch(loginUser(activeUser))
+      }
       dispatch(getQuestions(questions))
 
       dispatch(hideLoading())
